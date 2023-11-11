c72ceecf3e344ced76491a19572ed1ceac45d9c3
dubitable <pierrequereuil@gmail.com>
2023-11-11 17:28:27 -0500

    Switching Page Fix

--- matx/src/app/views/MainApp.jsx
+++ matx/src/app/views/MainApp.jsx
@@ -1,29 +1,39 @@
-import Analytics from "./dashboard/Analytics"
-import { useEffect, useState } from 'react';
+import Analytics from "./dashboard/Analytics";
+import Recommendations from "./recommendations/Recommendations";
+import { useEffect, useState } from "react";
 
-const MainApp = () => {
-    const [videos, setVideos] = useState([])
-    async function get_data() {
-    let hasMore = true
-    let cursor = "0"
+const url = "http://127.0.0.1:8000/tonedetection/getvideos?";
+
+const MainApp = (props) => {
+  const page = props.page.toLowerCase();
+
+  const [videos, setVideos] = useState([]);
+  async function get_data() {
+    let hasMore = true;
+    let cursor = "0";
     let vids = [];
 
-    while  (hasMore) {
-      const result = await fetch("http://127.0.0.1:8000/tonedetection/getvideos?" + new URLSearchParams({cursor}));
-      const json = await result.json()
-      vids = [...vids, ...json.data.videos]
-      const sortedVideos = vids.sort((a, b) => b.create_time - a.create_time)
-      setVideos([...sortedVideos]); 
-      hasMore = Boolean(json.data.hasMore)
-      cursor = json.data.cursor
-    } 
-
+    while (hasMore) {
+      console.log("fetching...");
+      const result = await fetch(url + new URLSearchParams({ cursor }));
+      const json = await result.json();
+      vids = [...vids, ...json.data.videos];
+      const sortedVideos = vids.sort((a, b) => b.create_time - a.create_time);
+      setVideos([...sortedVideos]);
+      hasMore = Boolean(json.data.hasMore);
+      cursor = json.data.cursor;
+    }
   }
 
   useEffect(() => {
-    get_data()
-  }, [])
-    return <Analytics videos = {videos}/>
-}
+    get_data();
+  }, []);
 
-export default MainApp
+  if (page === "recommendations") {
+    return <Recommendations videos={videos} />;
+  }
+
+  return <Analytics videos={videos} />;
+};
+
+export default MainApp;
